3b3012a5cab3c16b3b3d8db2e1af3f582a6c1ac7
lasse <lybeck.lasse@gmail.com>
2013-01-14 10:58:56 +0200

    First commit.

---
+++ src/tiralabra/TiRaLabra.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiralabra;
+
+/**
+ *
+ * @author lasse
+ */
+public class TiRaLabra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
