66267d616d6db0a722a2ed537af49ae5cc74dd9a
lasse <lybeck.lasse@gmail.com>
2013-01-23 15:19:10 +0200

    Began working on javadocs.

--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -5,26 +5,45 @@
 package matrix;
 
 /**
- *
+ * Class that represents a matrix.
+ * 
  * @author lasse
  */
 public class Matrix {
 
     protected double[][] data;
     protected final int rows, cols;
+    
     private static final double largeValueLimit = 1000000;
     private static final double smallValueLimit = 1 / largeValueLimit;
 
+    /**
+     * Creates a zero-valued matrix of size n x n.
+     * 
+     * @param n Number of rows and columns in the matrix.
+     */
     public Matrix(int n) {
         this(n, n);
     }
 
+    /**
+     * Creates a zero-valued matrix of size rows x cols.
+     * 
+     * @param rows Number of rows in the matrix.
+     * @param cols Number of columns in the matrix.
+     */
     public Matrix(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
         data = new double[rows][cols];
     }
 
+    /**
+     * Creates a matrix of the input.
+     * The matrix must have as many columns in every row.
+     * 
+     * @param matrix The matrix to be created.
+     */
     public Matrix(double[][] matrix) {
         if (!validMatrix(matrix)) {
             throw new IllegalArgumentException("All columns in matrix should be of same size!");
@@ -51,10 +70,20 @@ public class Matrix {
         return newMatrix;
     }
 
+    /**
+     *
+     * @param orig
+     */
     public Matrix(Matrix orig) {
         this(orig.data);
     }
 
+    /**
+     *
+     * @param rows
+     * @param cols
+     * @return
+     */
     public static Matrix ones(int rows, int cols) {
         Matrix m = new Matrix(rows, cols);
         for (int i = 0; i < m.rows; i++) {
@@ -65,10 +94,21 @@ public class Matrix {
         return m;
     }
 
+    /**
+     *
+     * @param n
+     * @return
+     */
     public static Matrix ones(int n) {
         return ones(n, n);
     }
 
+    /**
+     *
+     * @param rows
+     * @param cols
+     * @return
+     */
     public static Matrix eye(int rows, int cols) {
         Matrix m = new Matrix(rows, cols);
         for (int i = 0; i < Math.min(m.rows, m.cols); i++) {
@@ -77,10 +117,18 @@ public class Matrix {
         return m;
     }
 
+    /**
+     *
+     * @param n
+     * @return
+     */
     public static Matrix eye(int n) {
         return eye(n, n);
     }
 
+    /**
+     *
+     */
     public void print() {
         String formatString;
         if (containsLargeOrSmallValues()) {
@@ -109,62 +157,132 @@ public class Matrix {
         return false;
     }
 
+    /**
+     *
+     * @param i
+     * @param j
+     * @return
+     */
     public double at(int i, int j) {
         return data[i][j];
     }
 
+    /**
+     *
+     * @return
+     */
     public int getRows() {
         return rows;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getCols() {
         return cols;
     }
 
+    /**
+     *
+     * @return
+     */
     protected double[][] getData() {
         return data;
     }
 
+    /**
+     *
+     * @param k
+     * @return
+     */
     public Matrix scale(double k) {
         return Operations.scale(this, k);
     }
 
+    /**
+     *
+     * @param k
+     * @return
+     */
     public Matrix add(double k) {
         return Operations.add(this, k);
     }
 
+    /**
+     *
+     * @param m
+     * @return
+     */
     public Matrix add(Matrix m) {
         return Operations.add(this, m);
     }
 
+    /**
+     *
+     * @param m
+     * @return
+     */
     public Matrix sub(Matrix m) {
         return Operations.subtract(this, m);
     }
 
+    /**
+     *
+     * @param m
+     * @return
+     */
     public Matrix mul(Matrix m) {
         return mulNaive(m);
     }
 
+    /**
+     *
+     * @param m
+     * @return
+     */
     public Matrix mulNaive(Matrix m) {
         return Operations.mulNaive(this, m);
     }
 
+    /**
+     *
+     * @param m
+     * @return
+     */
     public Matrix mulStrassen(Matrix m) {
         return Operations.mulStrassen(this, m);
     }
 
+    /**
+     *
+     * @param e
+     * @return
+     */
     public Matrix pow(int e) {
         return Operations.pow(this, e);
     }
 
+    /**
+     *
+     * @return
+     */
     public Matrix transpose() {
         return Operations.transpose(this);
     }
 
+    /**
+     *
+     * @return
+     */
     public double det() {
         return Operations.det(this);
     }
 
+    /**
+     *
+     * @return
+     */
     public Matrix inv() {
         return Operations.inv(this);
     }
