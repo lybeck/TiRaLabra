30a59dd9182d82eebaf0fe8146132797b9ca5d29
Lasse <lybeck.lasse@gmail.com>
2013-02-04 19:17:04 +0200

    Worked on UserInterface. Parsing string to matrix now works, although code is still quite messy....

--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -59,13 +59,11 @@ public class UserInterface {
             case "vars":
                 printVariableNames();
                 break;
+            case "clear":
+                variables.clear();
+                break;
             default:
-                String[] split = command.split("\\s");
-                if (variables.containsKey(split[0])) {
-                    performVariableCommand(split);
-                } else {
-                    System.out.println("Unrecognised command!");
-                }
+                performVariableCommand(command);
         }
 
         return true;
@@ -79,6 +77,7 @@ public class UserInterface {
         String varName;
         while (true) {
             System.out.println("Input name for new variable:");
+            System.out.print(">> ");
             varName = scanner.nextLine();
             if (varName.length() < 1) {
                 System.out.println("Did not initialize new variable...");
@@ -124,33 +123,131 @@ public class UserInterface {
         }
     }
 
-    private void performVariableCommand(String[] split) {
-        Matrix var1 = variables.get(split[0]);
-        if (split.length == 1) {
-            if (var1 != null) {
-                var1.print();
-            } else {
-                System.out.println("(null)");
+    private void performVariableCommand(String command) {
+        Matrix result;
+        String varName = null;
+        if (command.contains("=")) {
+            String[] split = command.split("=");
+            if (split.length > 2) {
+                System.out.println("Too many '=' in command");
+                System.out.println("Command terminated.");
             }
-            return;
+            varName = split[0].trim();
+            if (!variables.containsKey(varName)) {
+                System.out.println("No variable named '" + varName + "' found! Check variables with command 'vars'.");
+                System.out.println("Command terminanted.");
+                return;
+            }
+            command = split[1];
         }
-
-        if (var1 == null) {
-            if (split[1].equals("=")) {
-                String[] newSplit = new String[split.length - 2];
-                System.arraycopy(split, 2, new String[split.length - 2], 2, split.length - 2);
-                var1 = calculateCommand(newSplit);
-                variables.put(split[0], var1);
-            } else {
-                System.out.println("Variable \"" + split[0] + "\" has a null value! Cannot perform command!");
-            }
-        } else {
+        result = calculateCommand(command);
+        if (varName != null && result != null) {
+            variables.put(varName, result);
         }
     }
 
-    private Matrix calculateCommand(String[] split) {
+    private Matrix calculateCommand(String command) {
+        if (command.length() < 1) {
+            System.out.println("Unrecoginzed command!");
+            return null;
+        }
+        boolean print = !command.endsWith(";");
+        if (!print) {
+            command = command.substring(0, command.length() - 1).trim();
+        }
+
+        if (variables.containsKey(command)) {
+            Matrix var = variables.get(command);
+            if (print) {
+                if (var != null) {
+                    var.print();
+                } else {
+                    System.out.println("(null)");
+                }
+            }
+            return var;
+        }
+
+        if (!command.endsWith(")")) {
+            System.out.println("Unrecognized command!");
+            return null;
+        }
+        command = command.substring(0, command.length() - 1);
+
+        String[] split = command.split("\\(");
+
+        if (split.length != 2) {
+            System.out.println("Unrecognized command!");
+            return null;
+        }
+
+        split[0] = split[0].toLowerCase().trim();
+        split[1] = split[1].toLowerCase().trim();
+
         Matrix result = null;
 
+        switch (split[0]) {
+            case "parse":
+                result = parseMatrix(split[1]);
+                break;
+        }
+
+        if (print && result != null) {
+            result.print();
+        }
         return result;
+    }
+
+    private Matrix parseMatrix(String string) {
+        if (!string.startsWith("[") || !string.endsWith("]")) {
+            System.out.println("Unparsable string!");
+            return null;
+        }
+        string = string.substring(1, string.length() - 1).trim();
+        String[] split = string.split(";");
+        int rows = split.length;
+        int cols = countColumns(split);
+        if (cols == -1) {
+            System.out.println("Unparsable string!");
+            return null;
+        }
+        double[][] m = new double[rows][cols];
+        try {
+            for (int i = 0; i < rows; i++) {
+                split[i] = split[i].replaceAll(",", " ");
+                Scanner row = new Scanner(split[i]);
+                for (int j = 0; j < cols; j++) {
+                    m[i][j] = row.nextDouble();
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Unparsable string!");
+            return null;
+        }
+        return new Matrix(m);
+    }
+
+    private int countColumns(String[] split) {
+        String regex = ",|\\s";
+        int cols = split[0].split(regex).length;
+        for (int i = 0; i < split.length; i++) {
+            if (cols != split[i].split(regex).length) {
+                return -1;
+            }
+        }
+        return cols;
+    }
+
+    private void printSplit(String[] split) {
+        System.out.println("Split:");
+        for (int i = 0; i < split.length; i++) {
+            System.out.println(i + ":");
+            System.out.println(split[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        new UserInterface().performVariableCommand("parse([0 2 3;2 1 1;5 2 5])");
     }
 }
